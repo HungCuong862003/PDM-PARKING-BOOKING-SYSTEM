a40721b7d016616f9f14a18c7a792d4fd1d64d87
Alexis <phucni2004@gmail.com>
2025-04-22 23:19:21 +0700

    create model Payment with attributes, getters, setters

---
+++ src/main/java/com/parkeasy/model/Payment.java
@@ -0,0 +1,73 @@
+package main.java.com.parkeasy.model;
+
+import java.sql.Date;
+
+public class Payment {
+    // primary key
+    private int paymentID;
+    // other attributes
+    private String paymentMethod;
+    private float amount;
+    private Date paymentDate;
+    // foreign key
+    private int reservationID;
+    private int cardNumber;
+
+    public Payment(int paymentID, String paymentMethod, float amount, Date paymentDate, int reservationID,
+            int cardNumber) {
+        this.paymentID = paymentID;
+        this.paymentMethod = paymentMethod;
+        this.amount = amount;
+        this.paymentDate = paymentDate;
+        this.reservationID = reservationID;
+        this.cardNumber = cardNumber;
+    }
+
+    public int getPaymentID() {
+        return paymentID;
+    }
+
+    public void setPaymentID(int paymentID) {
+        this.paymentID = paymentID;
+    }
+
+    public String getPaymentMethod() {
+        return paymentMethod;
+    }
+
+    public void setPaymentMethod(String paymentMethod) {
+        this.paymentMethod = paymentMethod;
+    }
+
+    public float getAmount() {
+        return amount;
+    }
+
+    public void setAmount(float amount) {
+        this.amount = amount;
+    }
+
+    public Date getPaymentDate() {
+        return paymentDate;
+    }
+
+    public void setPaymentDate(Date paymentDate) {
+        this.paymentDate = paymentDate;
+    }
+
+    public int getReservationID() {
+        return reservationID;
+    }
+
+    public void setReservationID(int reservationID) {
+        this.reservationID = reservationID;
+    }
+
+    public int getCardNumber() {
+        return cardNumber;
+    }
+
+    public void setCardNumber(int cardNumber) {
+        this.cardNumber = cardNumber;
+    }
+}
